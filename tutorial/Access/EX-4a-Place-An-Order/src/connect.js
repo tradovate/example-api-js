de88ec01d5de09be533dcc3fc947fd01930d95d8
Alexander <arlenross02@gmail.com>
2021-05-13 11:31:11 -0400

    DS-15 Incorrect import fix
    EX-4a should now work, changed bad import statement.

--- tutorial/Access/EX-4a-Place-An-Order/src/connect.js
+++ tutorial/Access/EX-4a-Place-An-Order/src/connect.js
@@ -1,6 +1,5 @@
-import { getAvailableAccounts } from '../../tutorial/Access/EX-4a-Place-An-Order/src/storage'
 import { DEMO_URL } from './env'
-import { setAccessToken, getAccessToken, tokenIsValid, setAvailableAccounts } from './storage'
+import { getAvailableAccounts, setAccessToken, getAccessToken, tokenIsValid, setAvailableAccounts } from './storage'
 
 const buildRequest = (data, ticket = '') => {
 
